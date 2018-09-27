e07a310d60249d24c51647a55e0437ce61e4c596
Lyn <lyn1990828@gmail.com>
2018-09-27 10:39:03 +0800

    fix bug

---
+++ src/main/java/com/i2bgod/test/web/model/UserModel.java
@@ -0,0 +1,13 @@
+package com.i2bgod.test.web.model;
+
+import lombok.Data;
+
+/**
+ * @auther: Lyn
+ * @data: 2018/9/25
+ */
+@Data
+public class UserModel {
+    private String username;
+    private String password;
+}
